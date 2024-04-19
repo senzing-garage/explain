762d17f722aed3f4292fe6c1010a72a159242ca1
docktermj <michael@dockter.com>
2024-04-19 15:29:07 -0400

    Fix generated code

--- cmd/github.go
+++ cmd/github.go
@@ -1,18 +1,3 @@
-// 0.2.1
-// Created by make-go-github-file.yaml on Fri Jan  5 15:34:40 UTC 2024
-//
-//lint:file-ignore U1000 Ignore all unused code, it's generated
-package cmd
-
-var (
-	githubDate           string = "2024-01-05"
-	githubIteration      string = "0"
-	githubRef            string = "refs/tags/0.2.1"
-	githubRefName        string = "0.2.1"
-	githubRepository     string = "senzing-garage/explain"
-	githubRepositoryName string = "explain"
-	githubVersion        string = "0.2.1"
-)
 // 0.2.2
 // Created by make-go-github-file.yaml on Fri Apr 19 18:34:46 UTC 2024
 //
